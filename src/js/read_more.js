4925460c1754110436987de0551c4f9001140dd9
Kirill Rudenkov <kiryll.rudenkov@yandex.by>
2023-12-01 21:36:34 +0300

    complite fix read_more

--- src/js/read_more.js
+++ src/js/read_more.js
@@ -1,19 +1,19 @@
 const btn = document.querySelector(".main__content-more");
-const more = document.getElementById("more");
 const text = document.querySelector(".main__content-expand-title");
 const img = document.querySelector(".main__content-expand-img");
-
+const hiddenText = document.getElementById("main__content-box");
 
 btn.addEventListener("click", () => {
     
     if (text.innerText =="Читать далее") {
         text.innerText = "Скрыть";
-        more.style.display = "inline";
-        img.src = "./assets/svg/expand2.svg";
+        img.classList.add("open");
+        hiddenText.classList.add('open');
     } else {
         text.innerText = "Читать далее";
-        more.style.display = "none";
-        img.src = "./assets/svg/expand.svg";
+        img.classList.remove("open");
+        hiddenText.classList.remove('open');
+
     }
 }
 );
